7fe1ea9f33cdf79dc5df403a404e366a2f003c25
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-01-04 16:43:42 +0000

    draft syntax for workflow

---
+++ scripts/main.R
@@ -0,0 +1,19 @@
+# gono model history matching
+# with emulation workflow
+#
+# see tidy models for inspiration
+# https://www.tmwr.org/workflows.html
+
+
+# create analysis object
+gono_hmer <- 
+  hmer_analysis() |> 
+  add_targets(names, data) |> 
+  add_input_ranges(names, data) |> 
+  add_model(gono_model)
+
+# run initial wave
+gono_hmer@run_wave
+
+# run first wave
+gono_hmer@run_wave
